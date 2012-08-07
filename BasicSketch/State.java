1f01087bd9eec462fe3fd66f891d3dadb5904459
Claude Falguiere <cfalguiere@gmail.com>
2012-08-07 17:35:09 +0200

    removed unused function

--- BasicSketch/State.java
+++ BasicSketch/State.java
@@ -3,18 +3,5 @@ class State {
     STARTED, SENDING, WAITING, RECEIVING, THINKING 
   }
   
-  
-  public String toString(StateValue state) {
-    String s = null;
-    switch (state) {
-      case STARTED:
-        s = "STARTED";
-      break;
-      default:
-        s = "TODO";
-      break;
-    }
-    return s;
-  }
 }
 
